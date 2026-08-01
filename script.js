5b66d153996a4e4800f30aac9102ca374bc6c6c6
atramentowy <riskycheese69@gmail.com>
2026-08-01 12:31:25 +0200

    feat: add working sections

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-const items = document.querySelectorAll('.projects li');
-const tooltip = document.getElementById('tooltip');
-const tooltipImg = document.getElementById('tooltipImg');
-const tooltipText = document.getElementById('tooltipText');
+function test() {
+	alert("Działa z pliku JS!");
+}
 
-items.forEach(item => {
-    item.addEventListener('mouseenter', function(e) {
-        const imgSrc = this.dataset.img;
-        const text = this.dataset.tooltip;
-        
-        if (imgSrc) tooltipImg.src = imgSrc;
-        if (text) tooltipText.textContent = text;
-        
-        tooltip.style.display = 'block';
+function showDiv(id) {
+	const forms = document.querySelectorAll('.section');
 
-      	const tooltipHeight = tooltip.offsetHeight || 200;
-        tooltip.style.left = (e.clientX + 20) + 'px';
-        tooltip.style.top = (e.clientY - tooltipHeight - 20) + 'px';
-        // tooltip.style.left = (e.clientX + 20) + 'px';
-        // tooltip.style.top = (e.clientY + 10) + 'px';
-    });
-    
-    item.addEventListener('mousemove', function(e) {
-        // tooltip.style.left = (e.clientX + 20) + 'px';
-        // tooltip.style.top = (e.clientY + 10) + 'px';
-        const tooltipHeight = tooltip.offsetHeight || 200;
-        tooltip.style.left = (e.clientX + 20) + 'px';
-        tooltip.style.top = (e.clientY - tooltipHeight - 20) + 'px';
-    });
-    
-    item.addEventListener('mouseleave', function() {
-        tooltip.style.display = 'none';
-    });
-});
+	forms.forEach(form => {
+		form.classList.add('hidden'); // show everything
+	});
+
+	document.getElementById(id).classList.remove('hidden'); // show one
+}
